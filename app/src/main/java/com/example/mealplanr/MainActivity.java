5e684cfb4e1101d49dc5d1f50d5011df0062adfe
JoshT <JoshT@amtimail.com>
2018-08-22 13:58:02 -0400

    continuing work with adding meals and keeping track of recipes.
    I think the right thing to move forward with is ArrayList<ArrayList<ArrayList<String>>>..
    
    The point being that the adapter can pass something like "Meal Name1", "Meal Name2", "etc..." and all of those spinner entries have a recipe associated with it (for tallying).

--- app/src/main/java/com/example/mealplanr/MainActivity.java
+++ app/src/main/java/com/example/mealplanr/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mealplanr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<ArrayList<String>> mealList = new ArrayList<>();
+
+        ArrayList<String> recipe1 = new ArrayList<>();
+
+        String s1 = "Meal Name!";
+        String s2 = "Ingredient 1";
+        String s3 = "Ingredient 2";
+        String s4 = "etc...";
+
+        recipe1.add(s1);
+        recipe1.add(s2);
+        recipe1.add(s3);
+        recipe1.add(s4);
+
+        mealList.add(recipe1);
+
+        Spinner s = findViewById(R.id.sunSpinner);
+        ArrayAdapter<ArrayList<String>> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mealList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
