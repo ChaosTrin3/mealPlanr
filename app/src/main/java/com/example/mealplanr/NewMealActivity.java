34f66d54d59313dd082a2100bdc457bd662a2e66
JoshT <JoshT@amtimail.com>
2018-08-23 12:48:03 -0400

    Wow, forgot to add the NewMealActivity.java last night. luckily I didn't do anything with it yet..

---
+++ app/src/main/java/com/example/mealplanr/NewMealActivity.java
@@ -0,0 +1,13 @@
+package com.example.mealplanr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewMealActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_meal);
+    }
+}
