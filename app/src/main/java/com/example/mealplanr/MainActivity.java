d362cb769f8cf9f33853b5097b1106e7879c6f00
JoshT <jdt015@bucknell.edu>
2018-08-22 20:55:37 -0400

    Added some code to open a new activity, generateNewMeal()
    I think the way moving forward will be to use the OnPause() and OnResume() methods to maintain a state of the meals that persist..
    
    I should also think about the spinners resetting values when the activities exchange..
    
    Overall, I don't think there is a great simple way to do this kind of data management (although there are a lot of ways that can get this done.)

--- app/src/main/java/com/example/mealplanr/MainActivity.java
+++ app/src/main/java/com/example/mealplanr/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mealplanr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,26 +16,41 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ArrayList<String>> mealList = new ArrayList<>();
+        ArrayList<String> mealList = new ArrayList<>();
 
-        ArrayList<String> recipe1 = new ArrayList<>();
+        String mealTitle1 = "Brisket, potatoes, and broccoli";
+        String mealTitle2 = "Salmon and Brussels over rice";
+        String mealTitle3 = "Chicken tray";
 
-        String s1 = "Meal Name!";
-        String s2 = "Ingredient 1";
-        String s3 = "Ingredient 2";
-        String s4 = "etc...";
+        mealList.add(mealTitle1);
+        mealList.add(mealTitle2);
+        mealList.add(mealTitle3);
 
-        recipe1.add(s1);
-        recipe1.add(s2);
-        recipe1.add(s3);
-        recipe1.add(s4);
-
-        mealList.add(recipe1);
-
-        Spinner s = findViewById(R.id.sunSpinner);
-        ArrayAdapter<ArrayList<String>> adapter = new ArrayAdapter<>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, mealList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
+
+        Spinner sun_s = findViewById(R.id.sunSpinner);
+        Spinner mon_s = findViewById(R.id.monSpinner);
+        Spinner tue_s = findViewById(R.id.tueSpinner);
+        Spinner wed_s = findViewById(R.id.wedSpinner);
+        Spinner thu_s = findViewById(R.id.thuSpinner);
+        Spinner fri_s = findViewById(R.id.friSpinner);
+        Spinner sat_s = findViewById(R.id.satSpinner);
+        sun_s.setAdapter(adapter);
+        mon_s.setAdapter(adapter);
+        tue_s.setAdapter(adapter);
+        wed_s.setAdapter(adapter);
+        thu_s.setAdapter(adapter);
+        fri_s.setAdapter(adapter);
+        sat_s.setAdapter(adapter);
+
+
+    }
+    /** Launches new Activity to customize a meal **/
+    public void generateNewMeal(View view){
+        Intent intent = new Intent(this, NewMealActivity.class);
+        startActivity(intent);
     }
 }
+
