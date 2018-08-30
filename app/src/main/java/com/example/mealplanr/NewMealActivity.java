cc4266d7a939caa030f1c5120b54c784e681b567
JoshT <jdt015@bucknell.edu>
2018-08-29 20:01:20 -0400

    The solution I'm moving forward with is a ScrollView-->TableLayout-->TableRow arrangement.
    I think I might be able to pass the contents of the TableLayout through an intent in one fowl swoop.
    
    I can't quite edit the table well, but I can figure something out next time.

--- app/src/main/java/com/example/mealplanr/NewMealActivity.java
+++ app/src/main/java/com/example/mealplanr/NewMealActivity.java
@@ -3,70 +3,57 @@ package com.example.mealplanr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class NewMealActivity extends Activity {
-
-    public List<String> ingredientList = new ArrayList<>();
-    public TableLayout i_tl;
-    public EditText ingredientName;
-    public Spinner qty;
-    public Spinner unit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meal);
 
-        final HashMap<String, ArrayList<String>> ingredientMap = new HashMap<>();
-
-        i_tl = findViewById(R.id.ingredientTableLayout);
-        ingredientName = findViewById(R.id.newIngredient);
-        qty = findViewById(R.id.qtySpinner);
-        unit = findViewById(R.id.unitsSpinner);
-
-        final TableRow ingredient = new TableRow(this);
-        ingredient.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-
-        TextView tv = new TextView(this);
-        tv.setText("New Data!");
-
-        ingredient.addView(tv,0);
-        ingredient.addView(tv, 1);
-
-        Button addBtn = findViewById(R.id.addIngredientBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        Button addBtn = findViewById(R.id.addIngredientBtn);                    //Initialize add button
+        addBtn.setOnClickListener(new View.OnClickListener() {                  //Overwrite the OnClickListener()
             @Override
             public void onClick(View v) {
-                ArrayList<String> ingredientInfo = new ArrayList<>();
-
-                ingredientList.add(ingredientName.getText().toString());
-                ingredientInfo.add(qty.getSelectedItem().toString());
-                ingredientInfo.add(unit.getSelectedItem().toString());
-
-                ingredientMap.put(ingredientName.getText().toString(), ingredientInfo);
-
-                i_tl.addView(ingredient, new TableLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-                ingredientName.setText("");
-                qty.setSelection(0);
-                unit.setSelection(0);
+                TableLayout tl = findViewById(R.id.ingredientTableLayout);      //Find the TableLayout view
+                ArrayList<String> ingredient = new ArrayList<>();               //Create a new ArrayList
+                EditText ingredient_name = findViewById(R.id.newIngredient);    //Find the view responsible for the ingredient name
+                Spinner ingredient_qty = findViewById(R.id.qtySpinner);         //Find the view responsible for the qty of the ingredient
+                Spinner ingredient_units = findViewById(R.id.unitsSpinner);     //Find the view responsible for the units of the qty
+                ingredient.add(ingredient_name.getText().toString());           //Add the name to the ingredient ArrayList
+                ingredient.add(ingredient_qty.getSelectedItem().toString());    //Add the qty to the ingredient ArrayList
+                ingredient.add(ingredient_units.getSelectedItem().toString());  //Add the unit to the ingredient ArrayList
+                TableRow ingredient_entry = newTableRow(ingredient);            //Generate a new TableRow for the ingredient
+                tl.addView(ingredient_entry);                                   //Add the TableRow to the TableLayout
+                ingredient_name.setText("");                                    //Reset the various inputs
+                ingredient_qty.setSelection(0);                                 //--
+                ingredient_units.setSelection(0);                               //--
             }
         });
+    }
+    public TableRow newTableRow(ArrayList<String> ingredientInfo){
+        TableRow entry = new TableRow(this);                            //Create a new TableRow
+        //Set LayoutParams()                                                //Define the TableRow LayoutParams()
+        TextView ingredient_name = new TextView(this);                  //Create a new TextView for the ingredient name
+        TextView ingredient_qty = new TextView(this);                   //Create a new TextView for the ingredient qty
+        TextView ingredient_units = new TextView(this);                 //Create a new TextView for the ingredient unit
+        ingredient_name.setWidth(800);                                      //Poorly managed LayoutParams workaround
+        ingredient_name.setText(ingredientInfo.get(0));                     //Populate the TextView
+        ingredient_qty.setWidth(80);                                        //Poorly managed LayoutParams workaround
+        ingredient_qty.setText(ingredientInfo.get(1));                      //Populate the TextView
+        ingredient_units.setWidth(200);                                     //Poorly managed LayoutParams workaround
+        ingredient_units.setText(ingredientInfo.get(2));                    //Populate the TextView
+        entry.addView(ingredient_name, 0);                              //Add the name to the TableRow
+        entry.addView(ingredient_qty, 1);                               //Add the qty to the TableRow
+        entry.addView(ingredient_units, 2);                             //Add the unit to the TableRow
+        return entry;                                                       //Return the new TableRow
     }
 }
